3db12035599f2ab1ae66e53a80bc901edc218a05
Julian <julian.battye@gmail.com>
2023-02-27 15:57:20 +0000

    Build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()]
+  integrations: [mdx(), tailwind(), react()],
+  site: "https://julianbattye.co.uk",
+  base: "/sessions",
 });
