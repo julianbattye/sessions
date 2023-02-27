719e654883c4eb593ba0fbb5d7ac9bddb71b7db7
Julian <julian.battye@gmail.com>
2023-02-27 19:42:24 +0000

    Weird text-sm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [mdx(), tailwind(), react()],
