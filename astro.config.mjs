8a44072e75351d7309f25dd9bcfb870bf5effbaf
Julian <julian.battye@gmail.com>
2023-03-09 14:18:36 +0000

    Attendance options

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), react()],
   site: "https://julianbattye.co.uk/sessions",
-  base: "sessions",
+  base: "sessions"
 });
